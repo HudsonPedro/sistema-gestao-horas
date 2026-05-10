5401b61dee4c1e3112be5d57460d5589632a63d9
HP <hudson.pedro@hotmail.com>
2026-05-10 19:19:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ st.markdown("""
         
         /* Ajusta cores do ERP */
         [data-testid="stSidebar"] { background-color: #f8f9fa; }
-        h1 { color: #da6744; } ##004a87
+        h1 { color: #da6744; } #(#004a87)
         
         /* Remove o espaçamento extra que o menu escondido deixa */
         div[data-testid="stSidebarUserContent"] { padding-top: 0rem; }
