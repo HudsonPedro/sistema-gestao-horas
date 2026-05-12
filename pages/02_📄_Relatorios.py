ba0449f76bccd1da35a9727cdb113b950b4afbbd
HP <hudson.pedro@hotmail.com>
2026-05-12 12:38:04 -0300

    Update 02_📄_Relatorios.py

--- pages/02_📄_Relatorios.py
+++ pages/02_📄_Relatorios.py
@@ -204,9 +204,9 @@ Hudson Valente"""
 
 # ====== STREAMLIT UI ======
 st.set_page_config(page_title="Gerador de Relatórios HPTECH", page_icon="📄", layout="wide")
-st.title(page_icon="hptechICO.png",
+st.title((page_icon="hptechICO.png",
     layout="wide",
-    initial_sidebar_state="expanded","🔥"Gerador Automático de Relatórios HPTECH")
+    initial_sidebar_state="expanded"),("🔥"Gerador Automático de Relatórios HPTECH"))
 st.markdown("---")
 
 st.set_page_config(
