793cfcdf93b48cdd844ff1c3a7e9c018bf22b448
HP <hudson.pedro@hotmail.com>
2026-05-12 18:55:33 -0300

    Change spreadsheet URL output format from drive to html

--- pages/01_📊_Dashboard.py
+++ pages/01_📊_Dashboard.py
@@ -108,7 +108,7 @@ VALOR_HORA = 80.00  # <--- ALTERE AQUI O VALOR DA SUA HORA
 def carregar_dados():
     #url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTtNKWayx3w7y8FPuV_hsaYWcZsB6ftUBKpJALkFOnlYxLEbNfu3LH0y76qxQsGhg/pub?output=xlsx"
     #url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSQABOlTPSx3-hKS7qPIXNl8jODyzQBF-_FVMR4JX3o0WNBmsl5OVPQUi0cNfZ1TMEShcH3hmHIL-kE/pub?output=xlsx"
-    url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSQABOlTPSx3-hKS7qPIXNl8jODyzQBF-_FVMR4JX3o0WNBmsl5OVPQUi0cNfZ1TMEShcH3hmHIL-kE/pub?output=drive"
+    url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSQABOlTPSx3-hKS7qPIXNl8jODyzQBF-_FVMR4JX3o0WNBmsl5OVPQUi0cNfZ1TMEShcH3hmHIL-kE/pub?output=html"
     
     dict_abas = pd.read_excel(url, sheet_name=None, engine='openpyxl')
     return dict_abas
