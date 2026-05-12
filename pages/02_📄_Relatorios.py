5c98411f71b433901a4e616f098bd16e500529d9
HP <hudson.pedro@hotmail.com>
2026-05-12 12:55:26 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- pages/02_📄_Relatorios.py
+++ pages/02_📄_Relatorios.py
@@ -204,7 +204,7 @@ Hudson Valente"""
 
 # ====== STREAMLIT UI ======
 st.set_page_config(page_title="Gerador de Relatórios HPTECH", page_icon="📄", layout="wide")
-st.image("hptechNova.png", use_container_width=True)
+st.image("hptechNova.png", layout="wide)
 st.title("🔥Gerador Automático de Relatórios HPTECH") #, page_icon="hptechICO.png", layout="wide", initial_sidebar_state="expanded")
 st.markdown("---")
 
