4f7cf2f83578044ff7fb6dbcfcac20701f3c5f78
HP <hudson.pedro@hotmail.com>
2026-05-13 13:02:26 -0300

    Add min_value and step to time inputs for desloc

--- pages/03_📝_Lancamento.py
+++ pages/03_📝_Lancamento.py
@@ -187,8 +187,8 @@ with st.form("form_lancamento", clear_on_submit=True):
         forma = st.selectbox("FORMA", ["Remoto", "Presencial"])
         
     with col3:
-        hr_inicio_d = st.time_input("HORA INICIO(Desloc)")
-        hr_fim_d = st.time_input("HORA FIM(Desloc)")
+        hr_inicio_d = st.time_input("HORA INICIO(Desloc), min_value=00:00, step=0.1")
+        hr_fim_d = st.time_input("HORA FIM(Desloc), min_value=00:00, step=0.1")
         km_d = st.number_input("KM(Desloc)", min_value=0.0, step=0.1)
         forma_d = st.text_input("FORMA(Desloc)")
 
