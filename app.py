1179a333b7e91886057bf138009ef5211d35a822
HP <hudson.pedro@hotmail.com>
2026-05-12 23:10:19 -0300

    Update page icon in app configuration

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import base64
 # 1. CONFIGURAÇÃO DA PÁGINA
 st.set_page_config(
     page_title="HPTECH Sistema de Gestão",
-    page_icon="hptechICO.png",
+    page_icon="hptech.png",
     layout="wide",
     initial_sidebar_state="expanded",
 )
