367b28b748738523f8de5e499db5607356762c51
HP <hudson.pedro@hotmail.com>
2026-05-16 10:35:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -93,6 +93,8 @@ with st.sidebar:
         st.switch_page("pages/03_📝_Lancamento.py")
     if st.button("💰 Medição Mensal", use_container_width=True):
         st.switch_page("pages/04_💰_Medicao_Mensal.py")
+    if st.button("📄Termos", use_container_width=True):
+        st.switch_page("pages/05_📄_Termos.py.py")
            
     st.divider()
     st.caption("v1.0 - 11052026")
