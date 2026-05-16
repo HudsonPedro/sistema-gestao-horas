5327d48e4bc9dd62e574426d5adf88dbf4bcb0b5
HP <hudson.pedro@hotmail.com>
2026-05-16 10:56:19 -0300

    Update URL for loading legends data

--- pages/05_📄_Termos.py
+++ pages/05_📄_Termos.py
@@ -67,7 +67,7 @@ st.write("Selecione o cliente e o módulo para gerar o documento customizado.")
 
 @st.cache_data(ttl=600)
 def carregar_legendas():
-    url = "https://google.com"
+    url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSQABOlTPSx3-hKS7qPIXNl8jODyzQBF-_FVMR4JX3o0WNBmsl5OVPQUi0cNfZ1TMEShcH3hmHIL-kE/pub?output=xlsx"
     df = pd.read_excel(url, sheet_name="Legendas", engine='openpyxl')
     return df
 
