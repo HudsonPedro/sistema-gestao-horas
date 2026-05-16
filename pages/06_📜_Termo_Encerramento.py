8e102e32c1e797d641eba845737c0f75c91f9276
HP <hudson.pedro@hotmail.com>
2026-05-16 15:03:57 -0300

    Update button for Termo Homologação and Termo Encerramento

--- pages/06_📜_Termo_Encerramento.py
+++ pages/06_📜_Termo_Encerramento.py
@@ -41,9 +41,9 @@ with st.sidebar:
         st.switch_page("pages/03_📝_Lancamento.py")
     if st.button("💰 Medição Mensal", use_container_width=True):
         st.switch_page("pages/04_💰_Medicao_Mensal.py")
-    if st.button("📋 Gerador de Termos", use_container_width=True): 
-        st.switch_page("pages/05_📋_Termos.py")
-    if st.button("📜 Termo Geral", use_container_width=True): 
+    if st.button("📋 Termo Homologação", use_container_width=True): 
+        st.switch_page("05_📑_Termos")
+    if st.button("📜 Termo Encerramento", use_container_width=True): 
         st.switch_page("06_📜_Termo_Encerramento.py")
       
     st.divider()
