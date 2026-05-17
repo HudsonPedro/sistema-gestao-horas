a30d2db7fcfcb80555b70fd1881101cd35821f7a
HP <hudson.pedro@hotmail.com>
2026-05-16 21:03:16 -0300

    Update URL for loading data in carregar_legendas

--- pages/06_📑_Termo_Encerramento.py
+++ pages/06_📑_Termo_Encerramento.py
@@ -37,7 +37,7 @@ st.markdown("""
 # 3. FUNÇÕES DE DADOS
 @st.cache_data(ttl=600)
 def carregar_legendas():
-    url = "https://google.com"
+    url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSQABOlTPSx3-hKS7qPIXNl8jODyzQBF-_FVMR4JX3o0WNBmsl5OVPQUi0cNfZ1TMEShcH3hmHIL-kE/pub?output=xlsx"
     df = pd.read_excel(url, sheet_name="Legendas", engine='openpyxl')
     return df
 
