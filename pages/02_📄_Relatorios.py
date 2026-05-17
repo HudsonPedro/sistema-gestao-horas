b000b6379cd6ee8b4eb14a07f1fcef7bd510d67d
HP <hudson.pedro@hotmail.com>
2026-05-17 11:05:58 -0300

    Update 02_📄_Relatorios.py

--- pages/02_📄_Relatorios.py
+++ pages/02_📄_Relatorios.py
@@ -344,22 +344,11 @@ if btn_gerar:
             participante_padrao = str(grupo["PARTICIPANTE"].iloc[0]).strip()
             local = str(grupo["LOCAL"].iloc[0]).strip()
 
-            #data_inicio_rel = grupo["DATA"].min().strftime("%d/%m/%Y")
-            data_inicio = grupo["DATA"].min()
-            if pd.notna(data_inicio):
-                data_inicio_rel = data_inicio.strftime("%d/%m/%Y")
-            else:
-                data_inicio_rel = ""
-            #data_fim_rel = grupo["DATA"].max().strftime("%d/%m/%Y")
-            data_fim = grupo["DATA"].max()
-            if pd.notna(data_fim):
-                data_fim_rel = data_fim.strftime("%d/%m/%Y")
-            else:
-                data_fim_rel = ""
+            data_inicio_rel = grupo["DATA"].min().strftime("%d/%m/%Y")
+            data_fim_rel = grupo["DATA"].max().strftime("%d/%m/%Y")
             dt_obj = grupo["DATA"].max().to_pydatetime()
             data_rodape = data_por_extenso_pt(dt_obj)
-            # == CORRIGIR O ERRO DATA ↑↑↑↑ ==#
-            
+                        
             # ------ CÁLCULO DE HORAS ------
             total_seg, total_seg_d = 0, 0
             
