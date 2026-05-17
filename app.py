75dea2cb3578836251b769f51f48e842945d59a1
HP <hudson.pedro@hotmail.com>
2026-05-17 18:26:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -155,13 +155,13 @@ with col4:
 with col5:
     st.subheader("📋 Homologação")
     st.write("Gere os Termos de Homologação e envie o lote por e-mail.")
-    if st.button("Novo Termo", key="btn_med"):
+    if st.button("Novo Termo", key="btn_termo_homolog"):
         st.switch_page("pages/05_📋_Termos.py")
 
 with col6:
     st.subheader("📑 Encerramento")
     st.write("Gere os Termos de Encerramento e envie o lote por e-mail.")
-    if st.button("Novo Encerramento", key="btn_med"):
+    if st.button("Novo Encerramento", key="btn_termo_encerra"):
         st.switch_page("pages/06_📑_Termo_Encerramento.py")        
 
 st.divider()
