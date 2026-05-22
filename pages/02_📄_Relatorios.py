309bdb4dcbf9231b4926b8bc267c59737880dd65
HP <hudson.pedro@hotmail.com>
2026-05-22 18:16:00 -0300

    Change 'Hello World' to 'Goodbye World'

--- pages/02_📄_Relatorios.py
+++ pages/02_📄_Relatorios.py
@@ -332,12 +332,11 @@ if btn_gerar:
 
          for col in cols_obr:
              if col not in df.columns: 
-             df[col] = ""
+                 df[col] = ""
          else: 
          # Impede que a coluna DATA seja convertida para string textualmente
-         if col != "DATA":
-             df[col] = df[col].fillna("").astype(str)
-
+             if col != "DATA":
+                 df[col] = df[col].fillna("").astype(str)
 
         df = df[df["RA"].str.strip() != ""]
         df = df[df["CLIENTE"].str.strip() != ""]
