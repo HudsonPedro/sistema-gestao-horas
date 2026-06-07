9df43f5b4ab1df6f1e4725fed509166d2356bc8d
HP <hudson.pedro@hotmail.com>
2026-06-07 12:03:28 -0300

    Update 02_📄_Relatorios.py

--- pages/02_📄_Relatorios.py
+++ pages/02_📄_Relatorios.py
@@ -217,7 +217,7 @@ st.markdown("---")
 @st.cache_data(ttl=600) 
 def carregar_planilha_todas_abas():
     url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSQABOlTPSx3-hKS7qPIXNl8jODyzQBF-_FVMR4JX3o0WNBmsl5OVPQUi0cNfZ1TMEShcH3hmHIL-kE/pub?output=xlsx"
-    dict_abas = pd.read_excel(url, sheet_name=None, engine='xlrd')
+    dict_abas = pd.read_excel(url, sheet_name=None, engine="openpyxl")
     return dict_abas
 
 st.sidebar.header("⚙️ Configurações GERAIS")
