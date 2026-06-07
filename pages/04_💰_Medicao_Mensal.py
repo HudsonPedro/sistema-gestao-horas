a6d890df426fe86dd113aed9751efa67274301d2
HP <hudson.pedro@hotmail.com>
2026-06-07 17:01:01 -0300

    Update 04_💰_Medicao_Mensal.py

--- pages/04_💰_Medicao_Mensal.py
+++ pages/04_💰_Medicao_Mensal.py
@@ -126,8 +126,6 @@ with st.sidebar:
         st.switch_page("pages/03_📝_Lancamento.py")
     if st.button("📄 Relatórios RA", use_container_width=True):
         st.switch_page("pages/02_📄_Relatorios.py")    
-    if st.button("💰 Medição Mensal", use_container_width=True):
-        st.switch_page("pages/04_💰_Medicao_Mensal.py")
     if st.button("📋 Termo Homologação", use_container_width=True): 
        st.switch_page("pages/05_📋_Termos.py")
     if st.button("📑 Termo Encerramento", use_container_width=True): 
