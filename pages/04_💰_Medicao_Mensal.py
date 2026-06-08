ed6fc795dc31454b1117e9e563b0d77cd5d62347
HP <hudson.pedro@hotmail.com>
2026-06-08 20:23:23 -0300

    Update 04_💰_Medicao_Mensal.py

--- pages/04_💰_Medicao_Mensal.py
+++ pages/04_💰_Medicao_Mensal.py
@@ -130,6 +130,8 @@ with st.sidebar:
        st.switch_page("pages/05_📋_Termos.py")
     if st.button("📑 Termo Encerramento", use_container_width=True): 
         st.switch_page("pages/06_📑_Termo_Encerramento.py")
+    if st.button("🚗 Termo Presencial", use_container_width=True): 
+        st.switch_page("pages/07_🚗_Termo_Treinamento_Presencial.py")
              
     
     st.divider()
