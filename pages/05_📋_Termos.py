b2ddcad4539235edb41aa73472994790b4251d48
HP <hudson.pedro@hotmail.com>
2026-06-08 20:23:38 -0300

    Add button for 'Termo Presencial' navigation

--- pages/05_📋_Termos.py
+++ pages/05_📋_Termos.py
@@ -173,6 +173,8 @@ with st.sidebar:
         st.switch_page("pages/04_💰_Medicao_Mensal.py")
     if st.button("📑 Termo Encerramento", use_container_width=True): 
         st.switch_page("pages/06_📑_Termo_Encerramento.py")
+    if st.button("🚗 Termo Presencial", use_container_width=True): 
+        st.switch_page("pages/07_🚗_Termo_Treinamento_Presencial.py")
     
     # INTERFACE DE DISPARO DA SIDEBAR
     st.markdown("---")
