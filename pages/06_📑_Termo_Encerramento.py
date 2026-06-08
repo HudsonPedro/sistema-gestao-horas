7c307ff323ef373af9ff18ea6a571c6b2ee14bd5
HP <hudson.pedro@hotmail.com>
2026-06-08 20:23:56 -0300

    Update 06_📑_Termo_Encerramento.py

--- pages/06_📑_Termo_Encerramento.py
+++ pages/06_📑_Termo_Encerramento.py
@@ -197,6 +197,8 @@ with st.sidebar:
         st.switch_page("pages/04_💰_Medicao_Mensal.py")
     if st.button("📋 Termo Homologação", use_container_width=True): 
        st.switch_page("pages/05_📋_Termos.py")
+    if st.button("🚗 Termo Presencial", use_container_width=True): 
+        st.switch_page("pages/07_🚗_Termo_Treinamento_Presencial.py")
         
     # INTERFACE DE DISPARO DA SIDEBAR
     st.markdown("---")
