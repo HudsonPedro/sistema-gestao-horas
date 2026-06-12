c6abecb835682f61b8982d4fd185be532aee60dd
HP <hudson.pedro@hotmail.com>
2026-06-12 17:15:32 -0300

    Update 02_📄_Relatorios.py

--- pages/02_📄_Relatorios.py
+++ pages/02_📄_Relatorios.py
@@ -839,10 +839,10 @@ if btn_gerar:
             ws.merge_range(f'F{row}:H{row}', solicitante, f_sign)
             row += 1
             ws.merge_range(f'A{row}:C{row}', "CRTI", f_center)
-            ws.write(f'A{row}:C{row}', f"RELATÓRIO DE ATENDIMENTO Nº {ra_str}", f_center)
-            row += 1
+            #ws.write(f'A{row}:C{row}', f"RELATÓRIO DE ATENDIMENTO Nº {ra_str}", f_center)
+            
             ws.merge_range(f'F{row}:H{row}', cliente, f_center)
-            ws.write(f'F{row}:H{row}', f"RELATÓRIO DE ATENDIMENTO Nº {ra_str}", f_center)
+            #ws.write(f'F{row}:H{row}', f"RELATÓRIO DE ATENDIMENTO Nº {ra_str}", f_center)
             row += 1
             
             ws.hide_gridlines(2)
