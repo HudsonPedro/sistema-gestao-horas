70f70b28885978b40fb5c88cb7100167ae03d17c
HP <hudson.pedro@hotmail.com>
2026-07-14 13:21:42 -0300

    Adjust column widths in app layout

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ if not st.session_state["autenticado"]:
                     st.error("❌ Usuário ou senha incorretos.")
                     
         st.markdown("<p style='text-align: center; color: #777; margin-top: 15px;'>Sistema Integrado Hptech.</p>", unsafe_allow_html=True)
-        _, col_centro, _ = st.columns([5, 20, 5])
+        _, col_centro, _ = st.columns([1, 100, 1])
         with col_centro:
             st.info("v1.1 - 14072026 | Copyright ©2026 HPtech Informática ME.", icon="ℹ️")
 
