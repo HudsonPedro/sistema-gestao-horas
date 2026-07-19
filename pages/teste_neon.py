e7f98355a004868510abf8c40b657cda9e112bba
HP <hudson.pedro@hotmail.com>
2026-07-19 19:36:04 -0300

    Update teste_neon.py

--- pages/teste_neon.py
+++ pages/teste_neon.py
@@ -1,5 +1,24 @@
 import streamlit as st
+import psycopg2
 
-st.title("Teste Secrets")
+st.title("Teste conexão Neon")
 
-st.write(st.secrets.keys())
+try:
+    conn = psycopg2.connect(
+        st.secrets["neon"]["DATABASE_URL"]
+    )
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT NOW();")
+    resultado = cursor.fetchone()
+
+    st.success("Conectado ao Neon com sucesso!")
+    st.write("Data do servidor:")
+    st.write(resultado)
+
+    conn.close()
+
+except Exception as e:
+    st.error("Erro na conexão:")
+    st.write(e)
