88c687ccad190d4a4b3b79fd40e4fc2bf9203eb9
HP <hudson.pedro@hotmail.com>
2026-07-19 17:47:58 -0300

    Create reset_admin.py

---
+++ pages/reset_admin.py
@@ -0,0 +1,20 @@
+import sqlite3
+import hashlib
+
+def criptografar_senha(senha):
+    return hashlib.sha256(senha.encode()).hexdigest()
+
+conn = sqlite3.connect("usuarios_sistema.db")
+cursor = conn.cursor()
+
+nova_senha = "Admin@123"
+
+cursor.execute(
+    "UPDATE usuarios SET senha_hash=? WHERE username='admin'",
+    (criptografar_senha(nova_senha),)
+)
+
+conn.commit()
+conn.close()
+
+print("Senha do admin alterada com sucesso.")
